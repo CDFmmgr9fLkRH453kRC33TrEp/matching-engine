3c6561e766d10ce5feb9842e0a324e792575e173
Caidan Speth-McCrary <caidan@Caidans-MacBook-Pro.local>
2023-01-06 17:41:54 -0800

    added support for csv test data

--- main.rs
+++ main.rs
@@ -1,12 +1,24 @@
 use std::cmp;
 use uuid::Uuid;
 type Price = usize;
-#[derive(Debug, Clone, Copy)]
+
+// for loading csv test files
+use std::env;
+use std::error::Error;
+use std::ffi::OsString;
+use std::fs::File;
+use std::process::{self, Output};
+use serde::Deserialize;
+
+type Record = (String, String, Option<u64>, f64, f64);
+
+
+#[derive(Debug, Clone, Copy, Deserialize)]
 enum OrderType {
     Buy,
     Sell,
 }
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Deserialize)]
 enum TickerSymbol {
     AAPL,
 }
@@ -27,21 +39,22 @@ struct LimitLevel {
     orders: Vec<Order>,
 }
 
-#[derive(Debug)]
-struct OrderRequest {
-    trader: Trader,
-    symbol: TickerSymbol,
-    amount: u8,
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct OrderRequest {    
+    amount: i32,
     price: Price,
     order_type: OrderType,
+    trader_id: i32,
+    symbol: TickerSymbol,
 }
 
 #[derive(Debug)]
 struct Order {
     order_id: Uuid,
-    trader: Trader,
+    trader_id: i32,
     symbol: TickerSymbol,
-    amount: u8,
+    amount: i32,
     price: Price,
     order_type: OrderType,
 }
@@ -54,7 +67,7 @@ struct Trader {
 struct TradeResult {
     sell_trader: Trader,
     buy_trader: Trader,
-    amount: u8,
+    amount: i32,
     price: Price,
     symbol: TickerSymbol,
     trade_time: u8,
@@ -64,7 +77,7 @@ impl OrderBook {
         let order_id = Uuid::new_v4();
         let new_order = Order {
             order_id: order_id,
-            trader: new_order_request.trader,
+            trader_id: new_order_request.trader_id,
             symbol: new_order_request.symbol,
             amount: new_order_request.amount,
             price: new_order_request.price,
@@ -91,6 +104,17 @@ impl OrderBook {
         order_id
     }
 
+    fn handle_incoming_order_request (&mut self, new_order_request: &mut OrderRequest) -> Option<Uuid> {
+        match new_order_request.order_type {
+            OrderType::Buy => {
+                self.handle_incoming_buy(new_order_request)
+            }
+            OrderType::Sell => {
+                self.handle_incoming_sell( new_order_request)
+            }
+        }
+    }
+
     fn remove_order_by_uuid(
         &mut self,
         order_id: Uuid,
@@ -99,21 +123,21 @@ impl OrderBook {
     ) -> Option<Order> {
         match side {
             OrderType::Buy => {
-                let mut pointer = 0;
-                while pointer < self.buy_side_limit_levels[price].orders.len() {
-                    if self.buy_side_limit_levels[price].orders[pointer].order_id == order_id {
-                        return Some(self.buy_side_limit_levels[price].orders.remove(pointer));
+                let mut index = 0;
+                while index < self.buy_side_limit_levels[price].orders.len() {
+                    if self.buy_side_limit_levels[price].orders[index].order_id == order_id {
+                        return Some(self.buy_side_limit_levels[price].orders.remove(index));
                     }
-                    pointer += 1;
+                    index += 1;
                 }
             }
             OrderType::Sell => {
-                let mut pointer = 0;
-                while pointer < self.sell_side_limit_levels[price].orders.len() {
-                    if self.sell_side_limit_levels[price].orders[pointer].order_id == order_id {
-                        return Some(self.sell_side_limit_levels[price].orders.remove(pointer));
+                let mut index = 0;
+                while index < self.sell_side_limit_levels[price].orders.len() {
+                    if self.sell_side_limit_levels[price].orders[index].order_id == order_id {
+                        return Some(self.sell_side_limit_levels[price].orders.remove(index));
                     }
-                    pointer += 1;
+                    index += 1;
                 }
             }
         }
@@ -123,22 +147,47 @@ impl OrderBook {
         if sell_order.price <= self.current_high_buy_price {
             println!("Cross, beginning matching");
             let mut current_price_level = self.current_high_buy_price;
-            while (sell_order.amount > 0) & (current_price_level > sell_order.price){
+            while (sell_order.amount > 0) & (current_price_level > sell_order.price) {
                 let mut order_index = 0;
                 while order_index < self.buy_side_limit_levels[current_price_level].orders.len() {
-                    let order_considering = &mut self.buy_side_limit_levels[current_price_level].orders[order_index];
-                    let amount_to_be_traded = cmp:min(sell_order.amount, order_considering.amount);
-                    println!("Match found, trading {:?} lots of {:?} @ ${:?}",amount_to_be_traded, sell_order.symbol, sell_order.price);
+                    let order_considering =
+                        &mut self.buy_side_limit_levels[current_price_level].orders[order_index];
+                    let amount_to_be_traded = cmp::min(sell_order.amount, order_considering.amount);
+                    println!(
+                        "Match found, trading {:?} lots of {:?} @ ${:?}",
+                        amount_to_be_traded, sell_order.symbol, sell_order.price
+                    );
+                    // TODO: create "sell" function that can handle calls to allocate credit etc.
+                    sell_order.amount -= amount_to_be_traded;
+                    order_considering.amount -= amount_to_be_traded;
+                    if order_considering.amount == 0 {
+                        self.sell_side_limit_levels[current_price_level]
+                            .orders
+                            .remove(order_index);
+                    }
+                    order_index += 1;
                 }
-
+                current_price_level += 1;
             }
+            while current_price_level < self.buy_side_limit_levels.len() {
+                if self.buy_side_limit_levels[current_price_level]
+                    .orders
+                    .len()
+                    > 0
+                {
+                    self.current_high_buy_price = current_price_level;
+                    break;
+                }
+                current_price_level += 1;
+            }
+            self.current_high_buy_price = current_price_level;
         }
+
         if sell_order.amount > 0 {
             return Some(self.add_order_to_book(sell_order));
         } else {
             return None;
         }
-
     }
     fn handle_incoming_buy(&mut self, buy_order: &mut OrderRequest) -> Option<Uuid> {
         if buy_order.price >= self.current_low_sell_price {
@@ -194,58 +243,60 @@ impl OrderBook {
         //     buy_order.trader, buy_order.amount, symbol, sell_trader, price
         // );
     }
+
+    fn vizualize_book_state(&self) -> Vec<i32>{
+        let mut book_ouput : Vec<i32> = vec![0; self.buy_side_limit_levels.len()];
+        for price_level_index in 0 .. self.buy_side_limit_levels.len() {
+            let mut outstanding_orders: i32 = 0;
+            for order in self.sell_side_limit_levels[price_level_index].orders.iter(){
+                outstanding_orders += order.amount;
+            }
+            for order in self.buy_side_limit_levels[price_level_index].orders.iter(){
+                outstanding_orders += order.amount;
+            }
+            book_ouput[price_level_index] = outstanding_orders;
+        };
+        book_ouput
+    }
+
+    fn load_csv_test_data(&mut self, file_path: OsString) -> Result<(), Box<dyn Error>> {
+        println!("Loading order data from {:?}", file_path);
+        let file = File::open(file_path)?;
+        let mut rdr = csv::Reader::from_reader(file);
+        for result in rdr.deserialize() {
+            let mut new_order_request: OrderRequest = result?;
+            self.handle_incoming_order_request(&mut new_order_request);
+        }
+        Ok(())
+    }
+}
+
+fn quickstart_order_book (symbol: TickerSymbol, min_price: Price, max_price:Price) -> OrderBook {
+    OrderBook {
+        symbol: TickerSymbol::from(symbol),
+        buy_side_limit_levels: (min_price..max_price)
+            .map(|x| LimitLevel {
+                price: x,
+                orders: Vec::new(),
+            })
+            .collect(),
+        sell_side_limit_levels:(min_price..max_price)
+            .map(|x| LimitLevel {
+                price: x,
+                orders: Vec::new(),
+            })
+            .collect(),
+        current_high_buy_price: min_price,
+        current_low_sell_price: max_price,
+    }
 }
 
 fn main() {
-    let mut order_book = OrderBook {
-        symbol: TickerSymbol::AAPL,
-        buy_side_limit_levels: (0..10)
-            .map(|x| LimitLevel {
-                price: x,
-                orders: Vec::new(),
-            })
-            .collect(),
-        sell_side_limit_levels: (0..10)
-            .map(|x| LimitLevel {
-                price: x,
-                orders: Vec::new(),
-            })
-            .collect(),
-        current_high_buy_price: 0,
-        current_low_sell_price: 255,
-    };
-    let mut test_buy_order = OrderRequest {
-        trader: Trader {
-            id: ['t', 'e', 's', 't'],
-        },
-        symbol: TickerSymbol::AAPL,
-        amount: 15,
-        price: 6,
-        order_type: OrderType::Buy,
-    };
-    let mut test_sell_order = OrderRequest {
-        trader: Trader {
-            id: ['t', 'e', 's', 't'],
-        },
-        symbol: TickerSymbol::AAPL,
-        amount: 5,
-        price: 4,
-        order_type: OrderType::Sell,
-    };
-    let mut test_sell_order_2 = OrderRequest {
-        trader: Trader {
-            id: ['t', 'e', 's', 't'],
-        },
-        symbol: TickerSymbol::AAPL,
-        amount: 5,
-        price: 5,
-        order_type: OrderType::Sell,
-    };
-
-    let sell_order_uuid = order_book.add_order_to_book(&test_sell_order_2);
-    let sell_order_uuid = order_book.add_order_to_book(&test_sell_order);
-    let buy_order_uuid = order_book.handle_incoming_buy(&mut test_buy_order);
-    // let _removed_order =
-    // order_book.remove_order(order_uuid, test_order.price, test_order.order_type);
-    println!("{:#?}", order_book);
+    let mut order_book = quickstart_order_book(TickerSymbol::AAPL, 0, 10);
+    if let Err(err) = order_book.load_csv_test_data("test_orders.csv".into()) {
+        println!("{}", err);
+        process::exit(1);
+    }
+    println!("{:?}", order_book.vizualize_book_state());
+    // println!("{:#?}", order_book);
 }
