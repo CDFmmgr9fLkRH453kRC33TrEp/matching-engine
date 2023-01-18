a36244a175fbbfaf3e8041e582be7e7e4e19550c
Caidan Speth-McCrary <caidan@Caidans-MacBook-Pro.local>
2023-01-17 20:15:22 -0500

    fixing orderbook bug, adding testing

---
+++ misc_test/client_test.py
@@ -0,0 +1,22 @@
+import requests
+import csv
+# {\"Amount\":10,\"Price\":1,\"OrderType\":\"Buy\",\"TraderId\":1,\"Symbol\":\"AAPL\"}
+with open('../src/test_orders.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    _h = 0
+    for row in spamreader: 
+        if _h == 0:
+            _h += 1
+            continue       
+        jsonreq = {
+            'OrderType': row[0],
+            'Amount': int(row[1]),
+            'Price': int(row[2]),
+            'Symbol': row[3],
+            'TraderId': int(row[4]),            
+        }
+        r = requests.post(url="http://127.0.0.1:3000", json=jsonreq)
+
+
+# sending post request and saving response as response object
+
