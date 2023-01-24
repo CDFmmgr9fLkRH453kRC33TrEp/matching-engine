f57d6e963b8624e92bd987cbf147c23289f8be10
Caidan Speth-McCrary <caidan@Caidans-MacBook-Pro.local>
2023-01-23 19:17:50 -0500

    added account balances, fill event handler

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ pub use crate::orderbook::OrderType;
 pub use crate::orderbook::OrderRequest;
 pub use crate::orderbook::CancelRequest;
 pub use crate::orderbook::quickstart_order_book;
+pub use crate::accounts::quickstart_trader_account;
 
 use macro_calls::TickerSymbol;
 use macro_calls::AssetBalances;
@@ -20,28 +21,35 @@ use macro_calls::AssetBalances;
 use macro_calls::GlobalOrderBookState;
 use macro_calls::GlobalAccountState;
 
-// impl GlobalOrderBookState {
-//     pub fn index_ref (&self, symbol:&TickerSymbol) -> &Mutex<crate::orderbook::OrderBook>{
-//         match symbol {
-//             $($name => {&self.$name}, )*
-//         }
-//     }
-//     pub fn index_ref_mut (&mut self, symbol:&TickerSymbol) -> &mut Mutex<crate::orderbook::OrderBook>{
-//         match symbol {
-//             $($name => { &mut self.$name}, )*
-//         }
-//     }
-// }
+
+struct GlobalState {
+    orderbook_state:macro_calls::GlobalOrderBookState,
+    account_state: macro_calls::GlobalAccountState
+}
 
 
-async fn add_order(order_request: web::Json<orderbook::OrderRequest>, data: web::Data<macro_calls::GlobalOrderBookState>) -> String {
+async fn add_order(order_request: web::Json<orderbook::OrderRequest>, data: web::Data<macro_calls::GlobalOrderBookState>, accounts_data: web::Data<macro_calls::GlobalAccountState>) -> String {
     let order_request_inner = order_request.into_inner();
     let symbol = &order_request_inner.symbol;
+    
+    if (order_request_inner.order_type == orderbook::OrderType::Buy) {
+        // ISSUE: This should decrement cents_balance to avoid racing to place two orders before updating cents_balance
+        // check if current cash balance - outstanding orders supports order
+        // nevermind, as long as I acquire and hold a lock during the entire order placement attempt, it should be safe                
+        let cent_value = &order_request_inner.amount * &order_request_inner.price;            
+        if (accounts_data.index_ref(order_request_inner.trader_id).lock().unwrap().cents_balance < cent_value) {
+            return String::from("Error Placing Order: The total value of order is greater than current account balance");
+        }  
+    };
     let orderbook = data.index_ref(symbol);
-    let order_id =  data.index_ref(symbol).lock().unwrap().handle_incoming_order_request(order_request_inner);
+    // ISSUE: need to borrow accounts as mutable without knowing which ones will be needed to be borrowed
+    // maybe pass in immutable reference to entire account state, and only acquire the locks for the mutex's that it turns out we need
+    let order_id =  data.index_ref(symbol).lock().unwrap().handle_incoming_order_request(order_request_inner, accounts_data);
     orderbook.lock().unwrap().print_book_state();
     match order_id {
-        Some(inner) => return inner.hyphenated().to_string(),
+        Some(inner) => {
+            return inner.hyphenated().to_string()
+        },
         None => String::from("Filled"),
     }
 }
@@ -63,19 +71,25 @@ async fn main() -> std::io::Result<()> {
     // to do: add actix guards to confirm correctly formed requests etc. 
     // to do: add actix guards to confirm credit checks etc. 
     // to do: move this declaration to macro_calls file to generate fields automatically
-    let global_order_book_state = web::Data::new(macro_calls::GlobalOrderBookState {
+    let global_orderbook_state = web::Data::new(macro_calls::GlobalOrderBookState {
         AAPL: Mutex::new(quickstart_order_book(macro_calls::TickerSymbol::AAPL, 0, 11)), 
         JNJ: Mutex::new(quickstart_order_book(macro_calls::TickerSymbol::JNJ, 0, 11)), 
+    });
+
+    let global_account_state = web::Data::new(macro_calls::GlobalAccountState {
+        Columbia_A: Mutex::new(accounts::quickstart_trader_account(macro_calls::TraderId::Columbia_A, 10)),
+        Columbia_B: Mutex::new(accounts::quickstart_trader_account(macro_calls::TraderId::Columbia_B, 10))
     });
 
     // let counter = web::Data::new(AppStateWithCounter {
     //     counter: Mutex::new(0),
     // });
-
+    // handlers discriminate based on type, so can safely pass both pieces of state here
     HttpServer::new(move || {
         App::new().service(
-            web::scope("/orders")
-            .app_data(global_order_book_state.clone()) // <- register the created data
+            web::scope("/orders")            
+            .app_data(global_orderbook_state.clone()) // <- register the created data
+            .app_data(global_account_state.clone()) // <- register the created data
             .route("/addOrder", web::post().to(add_order))
             .route("/cancelOrder", web::post().to(cancel_order)), 
         )
--- src/orderbook.rs
+++ src/orderbook.rs
@@ -1,13 +1,15 @@
+use crate::accounts;
 use crate::macro_calls;
 
-
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
 use std::cmp;
 use std::fmt;
 // use std::fmt::Display;
 use uuid::Uuid;
 pub type OrderID = uuid::Uuid;
 pub type Price = usize;
-pub type TraderId = u8;
+pub type TraderId = macro_calls::TraderId;
 // for loading csv test files
 // use std::env;
 use serde::{Deserialize, Serialize};
@@ -24,7 +26,7 @@ use log::{debug, error, info, trace, warn};
 // Importing csv
 type Record = (String, String, Option<u64>, f64, f64);
 
-#[derive(Debug, Clone, Copy, Deserialize, Serialize)]
+#[derive(Debug, Clone, Copy, Deserialize, Serialize, PartialEq)]
 pub enum OrderType {
     Buy,
     Sell,
@@ -69,7 +71,7 @@ struct LimitLevel {
 #[serde(rename_all = "PascalCase")]
 pub struct OrderRequest {
     /// Struct representing an incoming request which has not yet been added to the orderbook
-    pub amount: i32,
+    pub amount: usize,
     pub price: Price,
     pub order_type: OrderType,
     pub trader_id: TraderId,
@@ -82,7 +84,7 @@ pub struct Order {
     pub order_id: Uuid,
     pub trader_id: TraderId,
     pub symbol: macro_calls::TickerSymbol,
-    pub amount: i32,
+    pub amount: usize,
     pub price: Price,
     pub order_type: OrderType,
 }
@@ -105,7 +107,7 @@ struct Fill {
     /// Struct representing an order fill event, used to update credit limits, communicate orderbook status etc.
     sell_trader_id: TraderId,
     buy_trader_id: TraderId,
-    amount: i32,
+    amount: usize,
     price: Price,
     symbol: macro_calls::TickerSymbol,
     trade_time: u8,
@@ -194,13 +196,14 @@ impl OrderBook {
     pub fn handle_incoming_order_request(
         &mut self,
         new_order_request: OrderRequest,
+        accounts_data: web::Data<macro_calls::GlobalAccountState>
     ) -> Option<Uuid> {
         match new_order_request.order_type {
-            OrderType::Buy => self.handle_incoming_buy(new_order_request),
-            OrderType::Sell => self.handle_incoming_sell(new_order_request),
+            OrderType::Buy => self.handle_incoming_buy(new_order_request, accounts_data),
+            OrderType::Sell => self.handle_incoming_sell(new_order_request, accounts_data),
         }
     }
-    fn handle_incoming_sell(&mut self, mut sell_order: OrderRequest) -> Option<Uuid> {
+    fn handle_incoming_sell(&mut self, mut sell_order: OrderRequest, accounts_data: web::Data<macro_calls::GlobalAccountState>) -> Option<Uuid> {
         debug!(
             "Incoming sell, current high buy {:?}",
             self.current_high_buy_price
@@ -228,8 +231,10 @@ impl OrderBook {
                         sell_order.amount,
                         self.buy_side_limit_levels[current_price_level].orders[0].amount,
                     );
-
-                    self.handle_fill_event(Fill {
+                    
+                    let buy_trader = accounts_data.index_ref(buy_trader_id);
+                    let sell_trader = accounts_data.index_ref(sell_order.trader_id);
+                    self.handle_fill_event(buy_trader, sell_trader, Fill {
                         sell_trader_id: sell_order.trader_id,
                         buy_trader_id: buy_trader_id,
                         symbol: self.symbol,
@@ -237,7 +242,7 @@ impl OrderBook {
                         price: trade_price,
                         trade_time: 1,
                     });
-                    // TODO: create "sell" function that can handle calls to allocate credit etc.
+
                     sell_order.amount -= amount_to_be_traded;
                     self.buy_side_limit_levels[current_price_level].orders[0].amount -=
                         amount_to_be_traded;
@@ -274,7 +279,7 @@ impl OrderBook {
             return None;
         }
     }
-    fn handle_incoming_buy(&mut self, mut buy_order: OrderRequest) -> Option<Uuid> {
+    fn handle_incoming_buy(&mut self, mut buy_order: OrderRequest, accounts_data: web::Data<macro_calls::GlobalAccountState>) -> Option<Uuid> {
         debug!(
             "Incoming Buy, current low sell {:?}",
             self.current_low_sell_price
@@ -295,17 +300,19 @@ impl OrderBook {
                     );
                     let trade_price =
                         self.sell_side_limit_levels[current_price_level].orders[0].price;
-                    let buy_trader_id =
+                    let sell_trader_id =
                         self.sell_side_limit_levels[current_price_level].orders[0].trader_id;
 
                     let amount_to_be_traded = cmp::min(
                         buy_order.amount,
                         self.sell_side_limit_levels[current_price_level].orders[0].amount,
                     );
-
-                    self.handle_fill_event(Fill {
+                    // could turn this into an associated function which does not need a reference to the orderbook, but I think its fine.
+                    let buy_trader = accounts_data.index_ref(buy_order.trader_id);
+                    let sell_trader = accounts_data.index_ref(sell_trader_id);
+                    self.handle_fill_event(buy_trader, sell_trader, Fill {
                         buy_trader_id: buy_order.trader_id,
-                        sell_trader_id: buy_trader_id,
+                        sell_trader_id: sell_trader_id,
                         symbol: self.symbol,
                         amount: amount_to_be_traded,
                         price: trade_price,
@@ -313,6 +320,7 @@ impl OrderBook {
                     });
 
                     // TODO: create "sell" function that can handle calls to allocate credit etc.
+                    // also removing from the front seems pretty inefficient,
                     buy_order.amount -= amount_to_be_traded;
                     self.sell_side_limit_levels[current_price_level].orders[0].amount -=
                         amount_to_be_traded;
@@ -347,7 +355,15 @@ impl OrderBook {
         }
     }
 
-    fn handle_fill_event(&self, fill_event: Fill) {
+    fn handle_fill_event(&self, buy_trader: &Mutex<accounts::TraderAccount>, sell_trader: &Mutex<accounts::TraderAccount>, fill_event: Fill) {
+        let cent_value = &fill_event.amount * &fill_event.price;            
+        
+        *buy_trader.lock().unwrap().asset_balances.index_ref(&fill_event.symbol).lock().unwrap() -= fill_event.amount;
+        buy_trader.lock().unwrap().cents_balance += cent_value;
+        
+        *sell_trader.lock().unwrap().asset_balances.index_ref(&fill_event.symbol).lock().unwrap() += fill_event.amount;
+        sell_trader.lock().unwrap().cents_balance -= cent_value;
+
         debug!(
             "{:?} sells to {:?}: {:?} lots of {:?} @ ${:?}",
             fill_event.sell_trader_id,
@@ -360,8 +376,8 @@ impl OrderBook {
 
     pub fn print_book_state(&self) {
         for price_level_index in 0..self.buy_side_limit_levels.len() {
-            let mut outstanding_sell_orders: i32 = 0;
-            let mut outstanding_buy_orders: i32 = 0;
+            let mut outstanding_sell_orders: usize = 0;
+            let mut outstanding_buy_orders: usize = 0;
             for order in self.sell_side_limit_levels[price_level_index].orders.iter() {
                 outstanding_sell_orders += order.amount;
             }
@@ -383,16 +399,16 @@ impl OrderBook {
         }
     }
 
-    pub fn load_csv_test_data(&mut self, file_path: OsString) -> Result<(), Box<dyn Error>> {
-        info!("Loading order data from {:?}", file_path);
-        let file = File::open(file_path)?;
-        let mut rdr = csv::Reader::from_reader(file);
-        for result in rdr.deserialize() {
-            let new_order_request: OrderRequest = result?;
-            self.handle_incoming_order_request(new_order_request);
-        }
-        Ok(())
-    }
+    // pub fn load_csv_test_data(&mut self, file_path: OsString) -> Result<(), Box<dyn Error>> {
+    //     info!("Loading order data from {:?}", file_path);
+    //     let file = File::open(file_path)?;
+    //     let mut rdr = csv::Reader::from_reader(file);
+    //     for result in rdr.deserialize() {
+    //         let new_order_request: OrderRequest = result?;
+    //         self.handle_incoming_order_request(new_order_request);
+    //     }
+    //     Ok(())
+    // }
 }
 
 pub fn quickstart_order_book(
@@ -421,10 +437,10 @@ pub fn quickstart_order_book(
 
 fn main() {
     let mut order_book = quickstart_order_book(macro_calls::TickerSymbol::AAPL, 0, 11);
-    if let Err(err) = order_book.load_csv_test_data("src/test_orders.csv".into()) {
-        println!("{}", err);
-        process::exit(1);
-    }
+    // if let Err(err) = order_book.load_csv_test_data("src/test_orders.csv".into()) {
+    //     println!("{}", err);
+    //     process::exit(1);
+    // }
     order_book.print_book_state();
     // println!("{:#?}", order_book);
     // let o_req = OrderRequest{
