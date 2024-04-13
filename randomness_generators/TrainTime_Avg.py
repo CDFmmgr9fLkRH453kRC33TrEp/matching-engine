868b78d6162e70fcec3e7938d7f4138fefa1bf81
ZevMM <zevmcmanusmendelowitz@gmail.com>
2024-04-12 22:20:49 -0400

    Using arctan to map all train times to range (0,1)

--- randomness_generators/TrainTime_Avg.py
+++ randomness_generators/TrainTime_Avg.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 from PIL import Image
 import matplotlib.pyplot as plt
+import math
 #doesn't account for time = "now", but that ends up adding more variance
 
 file = open('Train_data','a+')
@@ -11,15 +12,26 @@ driver.get("https://realtimerail.nyc/stops/117")
 time.sleep(10)
 y_vals = []
 x_vals = []
+#if we actually want avg wait time, should use the difference between each time
 
-for i in range(5):
-    times = driver.find_elements(By.CLASS_NAME, value= "time")
-    avg = sum(int("".join([s for s in t.text if s.isdigit()])) for t in times)
+for i in range(60):
+    myl = []
+    for n in range(5):
+        times = driver.find_elements(By.CLASS_NAME, value= "time")
+        if (n < len(times)):
+            myl.append(int("0" + "".join([s for s in times[n].text if s.isdigit()])))
+        else:
+            myl.append(0)
+    avg = sum(myl)   
+    ##avg = sum(int("0" + "".join([s for s in t.text if s.isdigit()])) for t in times)
     y_vals.append(avg)
     x_vals.append(i)
-    file.write(str(avg) + '\n')
+    runavg = sum(y_vals) / len(y_vals)
+    file.write(str(((math.atan((avg - runavg) / 6) /  (math.pi)) + 0.5)*10000) + '\n')
     driver.refresh()
     time.sleep(10)
+
+##((math.atan(avg) /  (math.pi)) + 0.5) * 10000
 
 file.close()
 plt.plot(x_vals, y_vals, label='Data Points', marker='o')
