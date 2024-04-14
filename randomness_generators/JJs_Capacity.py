70cdbd78b3ada7ac486c3919158688bf57319512
ZevMM <zevmcmanusmendelowitz@gmail.com>
2024-04-14 18:30:32 -0400

    Randomness generators update each minute, map to range (0,50)

--- randomness_generators/JJs_Capacity.py
+++ randomness_generators/JJs_Capacity.py
@@ -4,6 +4,7 @@ import time
 from PIL import Image
 import matplotlib.pyplot as plt
 import math
+import random
 
 file = open('JJs_data','a+')
 driver = webdriver.Edge()
@@ -13,18 +14,19 @@ driver.get("https://dining.columbia.edu/content/jjs-place-0")
 time.sleep(15)
 data = []
 
-for i in range(5):
+for i in range(60):
     capacity = driver.find_element(By.CLASS_NAME, value= "indicator").text
     end = capacity.index('%')
     capacity = capacity[:end]
     avg = float(capacity)
     data.append(avg)
     runavg = sum(data) / len(data)
-    file.write(str(((math.atan((avg - runavg)/3) /  (math.pi)) + 0.5)*10000) + '\n')
+    file.write(str(((math.atan((avg - runavg)/3) /  (math.pi)) + 0.5)*50) + '\n')
     #y_vals.append(float(capacity))
     #x_vals.append(i)
     driver.refresh()
-    time.sleep(20)
+    offset = random.randint(0,5)
+    time.sleep(60 + offset)
 
 file.close()
 
